e785eb8e945f6c682fcfccf57d69fd34f202bec6
Guillermo Alfaro <galfarotolon@gmail.com>
2021-01-04 17:35:51 -0500

    basic styling to trivia section, subject to change, just temporary styles

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Countries = () => {
     return (
         <div>
             Hello from Countries
+
+            <Link to='/'>Back Home</Link>
         </div>
     )
 }
--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Trivia = () => {
     return (
-        <div>
-            Hello
+        <div className='flex justify-center items-center h-screen flex-col'>
+            Welcome to the Trivia Section!
+
+            <Link to='/'>Home</Link>
+
+            <div className='text-purple-800 p-4'>
+
+                Question Here
+
+            </div>
+
+            <div className='answers grid grid-cols-2 gap-4 bg-red-200 p-10'>
+                <button className='bg-red-500 text-white p-5 m-5'>Answer 1</button>
+                <button className='bg-red-500 text-white p-5 m-5'>Answer 2</button>
+                <button className='bg-red-500 text-white p-5 m-5'>Answer 3</button>
+                <button className='bg-red-500 text-white p-5 m-5'>Answer 4</button>
+            </div>
         </div>
     )
 }
